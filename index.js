09afac47b9a06a4502a3c5a95b93f60e110d08f6
1170374975 <1170374975@qq.com>
2020-07-31 10:07:23 +0800

    修改一些内容

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.getElementById('btn').onclick = function () {
     console.log('按下按钮1');
 }
 document.querySelector('button').onclick = function () {
-    console.log('按下按钮2');
+    console.log('按下按钮222');
 }
